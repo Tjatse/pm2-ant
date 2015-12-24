a5b959ffbc33989af0e4b84cab53e7c59238c0f4
Tjatse <thisnamemeansnothing@gmail.com>
2015-12-24 14:52:58 +0800

    logger && daemonize

--- pm2-ant.js
+++ pm2-ant.js
@@ -2,80 +2,96 @@ var Monitor = require('./lib/monitor'),
   chalk = require('chalk'),
   path = require('path'),
   fs = require('fs'),
-  pkg = require('./package.json'),
-  Log = require('./lib/util/log');
+  _ = require('lodash'),
+  Log = require('./lib/util/log'),
+  pkg = require('./package.json');
 
-var confFile = './pm2-ant.ini';
-if (process.argv.length > 2) {
-  var confFile = process.argv[2];
-}
-confFile = path.resolve(__dirname, confFile);
+exports.start = function (options) {
+  process.title = 'pm2-ant slave';
+  options = options || {};
+  var confFile = options.confFile;
+  if (!confFile) {
+    confFile = './pm2-ant.ini';
+    if (process.argv.length > 2) {
+      confFile = process.argv[2];
+    }
+    confFile = path.resolve(__dirname, confFile);
 
-if (!fs.existsSync(confFile)) {
-  console.log(chalk.bgRed.white('ERROR'), chalk.bold(confFile), 'does not exist!');
-  return process.exit(0);
-}
-
-var monitor = Monitor({
-  confFile: confFile
-});
-
-var log, logConf = monitor.options.log;
-if (logConf) {
-  if (!logConf.file) {
-    logConf.date = false;
-    logConf.prefix = false;
+    if (!fs.existsSync(confFile)) {
+      console.error(chalk.bold(confFile), 'does not exist!');
+      return process.exit(0);
+    }
   }
-  log = Log(logConf);
-}
+  var monitor = Monitor({
+    confFile: confFile
+  });
 
-log._log({
-  level: Log.INFO,
-  date: false,
-  prefix: false
-}, chalk.cyan(
-  '\n' +
-  '█▀▀█ █▀▄▀█ █▀█ ░░ █▀▀█ █▀▀▄ ▀▀█▀▀ \n' +
-  '█░░█ █░▀░█ ░▄▀ ▀▀ █▄▄█ █░░█ ░░█░░ \n' +
-  '█▀▀▀ ▀░░░▀ █▄▄ ░░ ▀░░▀ ▀░░▀ ░░▀░░ \n'));
+  Log(monitor.options.log);
 
+  console.log(chalk.cyan(
+    '\n' +
+    '█▀▀█ █▀▄▀█ █▀█ ░░ █▀▀█ █▀▀▄ ▀▀█▀▀ \n' +
+    '█░░█ █░▀░█ ░▄▀ ▀▀ █▄▄█ █░░█ ░░█░░ \n' +
+    '█▀▀▀ ▀░░░▀ █▄▄ ░░ ▀░░▀ ▀░░▀ ░░▀░░ \n'));
+  monitor.run();
 
-monitor.setLogger(log);
-monitor.run();
+  process.on('SIGTERM', shutdown);
+  process.on('SIGINT', shutdown);
+  process.on('SIGHUP', restart);
+  process.on('uncaughtException', caughtException);
+  process.on('exit', exports.exitGraceful)
 
-process.on('SIGTERM', shutdown);
-process.on('SIGINT', shutdown);
-process.on('SIGHUP', restart);
-process.on('uncaughtException', caughtException);
+  function shutdown(code, signal) {
+    console.info('Shutting down....');
+    monitor.quit();
+    console.info('Both', chalk.bold('pm2-emitter'), 'and', chalk.bold('statsd dgram'), 'sockets are closed.');
+    console.info('Shutdown complete!');
+    exports.exitGraceful(code, '-f');
+  }
 
-function shutdown(code) {
-  log.info('Shutting down....');
-  monitor.quit();
-  log.info('Both', chalk.bold('pm2-emitter'), 'and', chalk.bold('statsd dgram'), 'sockets are closed.');
-  log.info('Shutdown complete!');
-  exitGraceful(code);
-}
+  function restart() {
+    if (process.send) {
+      process.send({
+        action: 'restart'
+      });
+    } else {
+      console.error('No IPC found, could not restart monitor, shutting down.');
+      shutdown(1);
+    }
+  }
 
-function restart() {
-  log.info('Restarting...');
-  if (process.send) {
-    process.send({
-      action: 'restart'
-    });
-  } else {
-    log.error('Could not restart monitor, shutting down.');
+  function caughtException(err) {
+    console.error(err.stack);
     shutdown(1);
   }
-}
+};
 
-function caughtException(err) {
-  log.error(err.stack);
-  shutdown(1);
-}
-
-function exitGraceful(code) {
+exports.exitGraceful = function exit(code, signal) {
   code = code || 0;
-  log.stream.write(' ', function () {
-    process.exit(code);
+  if (signal != '-f') {
+    console.debug('Slave has exited, code: ' + code + ', signal: ' + (signal || 'NULL'));
+  }
+  var fds = 0;
+
+  function tryToExit() {
+    if ((fds & 1) && (fds & 2)) {
+      process.exit(code);
+    }
+  }
+
+  [process.stdout, process.stderr].forEach(function (std) {
+    var fd = std.fd;
+    if (!std.bufferSize) {
+      fds = fds | fd;
+    } else {
+      std.write && std.write('', function () {
+        fds = fds | fd;
+        tryToExit();
+      });
+    }
   });
+  tryToExit();
+};
+if (path.basename(process.mainModule.filename, '.js') == 'pm2-ant') {
+  exports.start();
 }
